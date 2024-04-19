e8e18c4603a8e3afcf7794b91482d08bc533123a
Eugine <121113067+softdev629@users.noreply.github.com>
2024-04-19 01:45:41 -0300

    jwt authentication complete

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,62 @@
+import { Entity, Column, Index, BeforeInsert } from "typeorm";
+import bcrypt from "bcryptjs";
+import Model from "./model.entity";
+
+export enum RoleEnumType {
+  USER = "user",
+  ADMIN = "admin",
+}
+
+@Entity("users")
+export class User extends Model {
+  @Column()
+  name: string;
+
+  @Index("email_index")
+  @Column({
+    unique: true,
+  })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({
+    type: "enum",
+    enum: RoleEnumType,
+    default: RoleEnumType.USER,
+  })
+  role: RoleEnumType;
+
+  @Column({
+    default: "default.png",
+  })
+  photo: string;
+
+  @Column({
+    default: false,
+  })
+  verified: boolean;
+
+  toJSON() {
+    return {
+      ...this,
+      password: undefined,
+      verified: undefined,
+    };
+  }
+
+  // Hash password before saving to database
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 12);
+  }
+
+  // Validate password
+  static async comparePasswords(
+    candidatePassword: string,
+    hashedPassword: string
+  ) {
+    return await bcrypt.compare(candidatePassword, hashedPassword);
+  }
+}
